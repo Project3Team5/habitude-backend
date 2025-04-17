e902faa1b69de66396c16ca1c0705e1852d1b1c1
Dalia <130174549+dalibug@users.noreply.github.com>
2025-04-17 16:28:33 -0700

    Fix: Add full implementation of Observation and ObservationController

--- src/main/java/com/habitude/controllers/ObservationController.java
+++ src/main/java/com/habitude/controllers/ObservationController.java
@@ -1,2 +1,23 @@
-package com.habitude.controllers;public class ObservationController {
+package com.habitude.controllers;
+import com.habitude.model.Observation;
+import com.habitude.service.ObservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+
+@RestController
+public class ObservationController {
+    @Autowired
+    private ObservationService observationService;
+
+    @GetMapping("/api/observations/summary")
+    public Map<String, Object> getSummary() {
+        return observationService.getSummary();
+    }
+
+    @GetMapping("/api/observations")
+    public List<Observation> getAllObservations() {
+        return observationService.getAllObservations();
+    }
 }
