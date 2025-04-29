6113ba5441dd01fb5bd808f15d7e69201f97ada1
Dalia <130174549+dalibug@users.noreply.github.com>
2025-04-29 15:13:36 -0700

    Bug fix

--- src/main/java/com/habitude/controllers/ObservationController.java
+++ src/main/java/com/habitude/controllers/ObservationController.java
@@ -3,6 +3,7 @@ import com.habitude.model.Observation;
 import com.habitude.service.ObservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.*;
